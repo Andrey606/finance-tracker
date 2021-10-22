2e0198e7c45f6312573de2810594eaf5fc53faf4
Andrey606 <andreykuluev96@gmail.com>
2021-10-22 16:55:48 +0300

    save token secured

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_d2ad5a42140445f0bd7102cf5dba8ac0',
+      publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
       # secret_token: 'secret_token',
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
