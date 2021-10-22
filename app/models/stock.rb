4d109210b9c254321524f1e8fbf59b03af02fe22
Andrey606 <andreykuluev96@gmail.com>
2021-10-22 16:12:48 +0300

    api is working

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_d2ad5a42140445f0bd7102cf5dba8ac0',
+      # secret_token: 'secret_token',
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+
+    client.price(ticker_symbol)
+  end
+
 end
