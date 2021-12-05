7b424e3b82c1c1371f172cb6102cc0778557729c
Andrey606 <andreykuluev96@gmail.com>
2021-12-05 02:01:13 +0200

    wefewfwef

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # resolve to devise gem use extra parameters like username and lastname for registration or editing...
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end
